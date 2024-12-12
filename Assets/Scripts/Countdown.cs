203b813abcd04aff1d502e3e78bd30d995b4c175
CAVE <caveneurorehablab@gmail.com>
2024-12-12 16:34:07 +0000

    fix(Countdown): show Finish message only after self-report is submitted.

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -108,7 +108,7 @@ public class Countdown : MonoBehaviour
             }
 
         }
-        if (Manager.isLastScene && !isCountdownStarted)
+        if (Manager.isLastScene && !isCountdownStarted && !Manager.SAM_Canvas.enabled)
         {
             CountdownCanvas.GetComponent<Canvas>().enabled = true;
             CanvasPanel.GetComponent<Image>().enabled = true;
