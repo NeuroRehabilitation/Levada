e89a30bab6b6690f3d5e232f850774b63ee89630
CAVE <caveneurorehablab@gmail.com>
2024-12-27 12:07:29 +0000

    fix(Countdown): fix countdown cameras.

---
+++ Assets/StressBar_Manager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StressBar_Manager : MonoBehaviour
+{
+    private static StressBar_Manager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "Main_Menu_HMD")
+        {
+            GameObject[] cameras = GameObject.FindGameObjectsWithTag("MainCamera");
+            foreach (GameObject camera in cameras)
+            {
+                if (camera.GetComponent<Camera>().clearFlags == CameraClearFlags.Depth)
+                {
+                    gameObject.GetComponent<Canvas>().worldCamera = camera.GetComponent<Camera>();
+                }
+            }
+        }
+    }
+}
