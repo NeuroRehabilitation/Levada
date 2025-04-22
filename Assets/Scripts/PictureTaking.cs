ef1d9464cd49d1f5d2e876af5f8813d9d6e5cfae
Vitor Freitas <vitorhcfreitas@gmail.com>
2025-04-22 12:32:31 +0100

    feat: save picture metadata and added collider detector

--- Assets/Scripts/PictureTaking.cs
+++ Assets/Scripts/PictureTaking.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -19,14 +20,16 @@ public class PictureTaking : MonoBehaviour
 
     void Update()
     {
-        if(captureButton.action.WasPressedThisFrame()){
+        if (captureButton.action.WasPressedThisFrame())
+        {
             Debug.Log("capture button was pressed");
             //TakeScreenshot();
             StartCoroutine(CaptureFromHandView());
         }
     }
 
-    void TakeScreenshot(){
+    void TakeScreenshot()
+    {
         string screenshotPath = Application.persistentDataPath + "/Screenshot_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
         ScreenCapture.CaptureScreenshot(screenshotPath);
         Debug.Log("Screenshot saved to: " + screenshotPath);
@@ -62,9 +65,56 @@ public class PictureTaking : MonoBehaviour
         Destroy(rt);
         Destroy(tempCamGO);
 
-        string screenshotPath = Application.persistentDataPath + "/Screenshot_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+        string baseFilename = "Screenshot_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string screenshotPath = Application.persistentDataPath + "/" + baseFilename + ".png";
         System.IO.File.WriteAllBytes(screenshotPath, screenshot.EncodeToPNG());
 
         Debug.Log("Screenshot saved from hand view to: " + screenshotPath);
+
+        float detectionDistance = 30f;
+        float coneAngle = 30f;
+        List<string> detectedNames = new List<string>();
+        Collider[] nearbyColliders = Physics.OverlapSphere(handTransform.position, detectionDistance);
+
+        foreach (var col in nearbyColliders)
+        {
+            Vector3 directionToObject = (col.transform.position - handTransform.position).normalized;
+            float angle = Vector3.Angle(handTransform.forward, directionToObject);
+
+            if (angle < coneAngle)
+            {
+                detectedNames.Add(col.gameObject.name);
+            }
+        }
+
+        Debug.DrawRay(handTransform.position, handTransform.forward * detectionDistance, Color.red, 2f);
+
+
+        PictureMetadata metadata = new PictureMetadata
+        {
+            timestamp = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+            position = handTransform.position,
+            eulerRotation = handTransform.rotation.eulerAngles,
+            forwardVector = handTransform.forward,
+            detectedObjects = detectedNames.ToArray()
+        };
+
+
+        string json = JsonUtility.ToJson(metadata, true);
+
+        string metadataPath = Application.persistentDataPath + "/" + baseFilename + ".json";
+        System.IO.File.WriteAllText(metadataPath, json);
+
+        Debug.Log("Metadata saved to: " + metadataPath);
     }
+}
+
+[System.Serializable]
+public class PictureMetadata
+{
+    public string timestamp;
+    public Vector3 position;
+    public Vector3 eulerRotation;
+    public Vector3 forwardVector;
+    public string[] detectedObjects;
 }
