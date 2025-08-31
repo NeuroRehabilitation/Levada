c8b70d05e377b51c9cd746bcb6c12ebad7f4877a
Vitor Freitas <vitorhcfreitas@gmail.com>
2025-08-31 22:49:39 +0100

    fix: fixed some textures, speed when walking in place and some waypoints

--- Assets/Scripts/2m Step Test/_2mStepTest_Manager.cs
+++ Assets/Scripts/2m Step Test/_2mStepTest_Manager.cs
@@ -624,14 +624,14 @@ public class _2mStepTest_Manager : MonoBehaviour
 
             //if (!isLongWalkTest)
             {
-                height = CalculateKneeHeight() + (1 * (CalculateHipHeight() - CalculateKneeHeight()) / 6); //NEW
+                /*height = CalculateKneeHeight() + (1 * (CalculateHipHeight() - CalculateKneeHeight()) / 6); //NEW
                 minHeight = CalculateKneeHeight() + (0.5f * (CalculateHipHeight() - CalculateKneeHeight()) / 6); //NEW
-                maxHeight = CalculateKneeHeight() + (1.5f * (CalculateHipHeight() - CalculateKneeHeight()) / 6); //NEW
-                /*height = CalculateMidThighHeight();
+                maxHeight = CalculateKneeHeight() + (1.5f * (CalculateHipHeight() - CalculateKneeHeight()) / 6); //NEW*/
+                height = CalculateMidThighHeight();
                 //height += height / 6; 
                 //height = CalculateKneeHeight() + (3 * (CalculateHipHeight() - CalculateKneeHeight()) / 4);
                 minHeight = CalculateKneeHeight() + (1 * (CalculateHipHeight() - CalculateKneeHeight()) / 4);
-                maxHeight = CalculateKneeHeight() + (3 * (CalculateHipHeight() - CalculateKneeHeight()) / 4);*/
+                maxHeight = CalculateKneeHeight() + (3 * (CalculateHipHeight() - CalculateKneeHeight()) / 4);
                 //float height = CalculateMidThighHeight();
                 _targetHeight.SetHeight(height);
                 //Debug.Log(height);
